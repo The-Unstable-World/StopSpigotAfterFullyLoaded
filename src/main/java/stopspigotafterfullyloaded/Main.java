a51a932b00f37cd8cc1874efd79bcfdea5abc23f
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-02-26 22:29:31 +0800

    Update Main.java

--- src/main/java/stopspigotafterfullyloaded/Main.java
+++ src/main/java/stopspigotafterfullyloaded/Main.java
@@ -1,4 +1,6 @@
 package stopspigotafterfullyloaded;
-public class Main extends org.bukkit.plugin.java.JavaPlugin {
+public class Main extends org.bukkit.plugin.java.JavaPlugin implements org.bukkit.event.Listener {
   @Override
-  public void onEnable() { getServer().shutdown(); }}
+  public void onEnable() { getServer().getPluginManager().registerEvents(this, this); }
+  @org.bukkit.event.EventHandler
+  public void onServerLoad(org.bukkit.event.server.ServerLoadEvent event) { getServer().shutdown(); }}
