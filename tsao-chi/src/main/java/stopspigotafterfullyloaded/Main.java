cc87de40b4f34ba380ab43d2428c65637c4bddea
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-02-26 22:00:23 +0800

    Create Main.java

---
+++ tsao-chi/src/main/java/stopspigotafterfullyloaded/Main.java
@@ -0,0 +1,7 @@
+package stopspigotafterfullyloaded;
+import org.bukkit.plugin.java.JavaPlugin;
+public class Main extends JavaPlugin {
+  @Override
+  public void onEnable() {
+    getServer().shutdown();
+}}
